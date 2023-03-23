de88c1ea09def67dbf5cf25a76f2cb29db1b7d95
anastasia <skityova.a.d@edu.mirea.ru>
2023-03-23 21:14:57 +0300

    Add binding

--- app/src/main/java/com/corylab/wcalendar/MainActivity.java
+++ app/src/main/java/com/corylab/wcalendar/MainActivity.java
@@ -5,32 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.corylab.wcalendar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        TextView titleText = (TextView) findViewById(R.id.title_text);
-        titleText.setText("Женский календарь");
-
-        EditText editText = (EditText) findViewById(R.id.editText);
-        editText.setHint("Введите текст");
-
-        ImageView flowerCircle = (ImageView) findViewById(R.id.flower_circle);
-        flowerCircle.setImageResource(R.drawable.img_1);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setText(R.string.main_button);
-
-        Button intentButton = (Button) findViewById(R.id.intent_button);
-        intentButton.setOnClickListener(new View.OnClickListener() {
+        binding.titleText.setText("Женский календарь");
+        binding.editText.setHint("Введите текст");
+        binding.grassImage.setImageResource(R.drawable.img_1);
+        binding.button.setText(R.string.main_button);
+        binding.intentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i("clickButton","Кнопка нажата 2");
