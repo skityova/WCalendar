92e652e4af112bc8b425f7ad4bafcfccb2be9a4e
anastasia <skityova.a.d@edu.mirea.ru>
2023-03-23 21:07:46 +0300

    Add log

--- app/src/main/java/com/corylab/wcalendar/MainActivity.java
+++ app/src/main/java/com/corylab/wcalendar/MainActivity.java
@@ -3,6 +3,8 @@ package com.corylab.wcalendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,13 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView titleText = (TextView) findViewById(R.id.title_text);
         titleText.setText("Женский календарь");
+
         EditText editText = (EditText) findViewById(R.id.editText);
         editText.setHint("Введите текст");
+
         ImageView flowerCircle = (ImageView) findViewById(R.id.flower_circle);
         flowerCircle.setImageResource(R.drawable.img_1);
+
         Button button = (Button) findViewById(R.id.button);
         button.setText(R.string.main_button);
+
+        Button intentButton = (Button) findViewById(R.id.intent_button);
+        intentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("clickButton","Кнопка нажата 2");
+            }
+        });
+    }
+
+    public void onButtonClick(View view) {
+        Log.i("clickButton","Кнопка нажата");
     }
 }
