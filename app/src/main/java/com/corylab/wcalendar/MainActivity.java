504c3dc750109d2add9f890ce019494b628307eb
anastasia <skityova.a.d@edu.mirea.ru>
2023-03-23 20:54:18 +0300

    Add work with resources

--- app/src/main/java/com/corylab/wcalendar/MainActivity.java
+++ app/src/main/java/com/corylab/wcalendar/MainActivity.java
@@ -3,6 +3,10 @@ package com.corylab.wcalendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView titleText = (TextView) findViewById(R.id.title_text);
+        titleText.setText("Женский календарь");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setHint("Введите текст");
+        ImageView flowerCircle = (ImageView) findViewById(R.id.flower_circle);
+        flowerCircle.setImageResource(R.drawable.img_1);
+        Button button = (Button) findViewById(R.id.button);
+        button.setText(R.string.main_button);
     }
 }
